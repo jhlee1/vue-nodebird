9cc1015a904e1154d5af8535220da2f86e7b9828
Joohan Lee <joohan33@gmail.com>
2020-03-31 15:23:52 +0900

    Added more note for login

--- ch04/back/app.js
+++ ch04/back/app.js
@@ -13,14 +13,20 @@ db.sequelize.sync({ force: true }); // 실무에선 쓰지말고 migration 권�
 passportConfig();
 
 app.use(morgan('dev'));
-app.use(cors('http://localhost:3000'));
+app.use(cors({
+    origin: 'http://localhost:3000', credential: true
+}));
 app.use(express.json()); // 이게 있어야만 request body를 json으로 받을 수 있음
 app.use(express.urlencoded({extended: false})); // Form에서 Action을 통해서 전송할 때 그 데이터를 해석해서 req.body에 넣어줌
 app.use(cookie('cookiesecret'));
 app.use(session({
     resave: false,
     saveUninitialized: false,
-    secret: 'cookiesecret'
+    secret: 'cookiesecret',
+    cookie: { // 브라우저에 connect.sid 쿠키로 저장됨
+        httpOnly: true,
+        secure: false
+    }
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -34,7 +40,7 @@ app.post('/user', async (req, res) => {
         const hash = await bcrypt.hash(req.body.password, 12); //salt 값이 높을수록 좋긴한데 컴퓨팅 파워를 많이 사용
         const exUser = await db.User.findOne({
             where: {
-            email: req.body.email
+                email: req.body.email
             }
         });
 
@@ -52,7 +58,7 @@ app.post('/user', async (req, res) => {
                 // password가 그대로 노출되기 때문에 암호화가 필요함: bcrypt, scrypt, pbkdf2 중 하나 사용하면 됨 일단 여기선 bcrypt를 쓸꺼임
                 // bcrypt를 javascript로 구현하면 너무 느리기 때문에 다른 언어(Python)로 만들어짐 => 설치하다가 에러가 많이 날 수 있음. 공식문서 참조하길 권장
                 // 윈도우의 경우, npm install --g -p windows-build-tools 명령어를 먼저 쓰고 설치하길 권장
-                password: hash,  
+                password: hash,
                 nickname: req.body.nickname
             }
         });
@@ -92,6 +98,11 @@ app.post('/user/login', (req, res) => {
     })(req, res, next);
 });
 
+app.post('/post', (req, res) => {
+    if (req.isAuthenticated()) {
+
+    }
+});
 
 app.listen(3085, () => {
     console.log(`백엔드 서버 ${3085}번 포트에서 작동중.`);
