2681b9e3a45c64744cf338174a68ac7a5f5b68dc
Joohan Lee <joohan33@gmail.com>
2020-04-06 09:28:17 +0900

    Debugging lecture code

--- ch04/back/app.js
+++ ch04/back/app.js
@@ -16,7 +16,7 @@ passportConfig();
 
 app.use(morgan('dev'));
 app.use(cors({
-    origin: 'http://localhost:3000', credential: true
+    origin: 'http://localhost:3000', credentials: true
 }));
 app.use(express.json()); // 이게 있어야만 request body를 json으로 받을 수 있음
 app.use(express.urlencoded({extended: false})); // Form에서 Action을 통해서 전송할 때 그 데이터를 해석해서 req.body에 넣어줌
@@ -45,6 +45,6 @@ app.listen(3085, () => {
 });
 
 
-// require과 module.exports, exports.XXX 
+// require과 module.exports, exports.XXX
 // 대표적인 것 = module.exports로 선언하고, 사소한 것은 exports.XXX로 선언하는 것이 좋음(각 함수별로 분해하기 쉽게)
 // 우선권은 module.exports가 더 높음
